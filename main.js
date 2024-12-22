79da7b1f6cdab0c9ed5e562b1b8dc02bb2dccf89
yiming.lien <yiming.lien@coretronic.com>
2024-12-22 22:09:10 +0800

    重構 Electron 應用程式，將主進程代碼移至 main.js，新增 preload.js 和 renderer.js，並更新 package.json 的主檔案設定

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('node:path')
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true,
+      preload: path.join(__dirname, 'preload.js'),
+      // nodeIntegration: true,
     },
   })
 
---
+++ renderer.js
@@ -0,0 +1,2 @@
+const information = document.getElementById('info')
+information.innerHTML = `This app is using Chrome(v${versions.chrome()}), Node(v${versions.node()}), and Electron(v${versions.electron()})`
